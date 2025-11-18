cff5ae347fc9f7426db8092624d33a48f3f81749
RITJustin <jdc7601@rit.edu>
2025-11-18 14:56:46 -0500

    Escape Room and it works!!!!!

---
+++ EscapeRoom/Source/IGME489ColeA5V2/IGME489ColeA5V2GameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IGME489ColeA5V2GameMode.h"
+
+AIGME489ColeA5V2GameMode::AIGME489ColeA5V2GameMode()
+{
+	// stub
+}
---
+++ EscapeRoom/Source/IGME489ColeA5V2/IGME489ColeA5V2PlayerController.h
@@ -0,0 +1,50 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "IGME489ColeA5V2PlayerController.generated.h"
+
+class UInputMappingContext;
+class UUserWidget;
+
+/**
+ *  Simple first person Player Controller
+ *  Manages the input mapping context.
+ *  Overrides the Player Camera Manager class.
+ */
+UCLASS(abstract)
+class IGME489COLEA5V2_API AIGME489ColeA5V2PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+
+	/** Constructor */
+	AIGME489ColeA5V2PlayerController();
+
+protected:
+
+	/** Input Mapping Contexts */
+	UPROPERTY(EditAnywhere, Category="Input|Input Mappings")
+	TArray<UInputMappingContext*> DefaultMappingContexts;
+
+	/** Input Mapping Contexts */
+	UPROPERTY(EditAnywhere, Category="Input|Input Mappings")
+	TArray<UInputMappingContext*> MobileExcludedMappingContexts;
+
+	/** Mobile controls widget to spawn */
+	UPROPERTY(EditAnywhere, Category="Input|Touch Controls")
+	TSubclassOf<UUserWidget> MobileControlsWidgetClass;
+
+	/** Pointer to the mobile controls widget */
+	TObjectPtr<UUserWidget> MobileControlsWidget;
+
+	/** Gameplay initialization */
+	virtual void BeginPlay() override;
+
+	/** Input mapping context setup */
+	virtual void SetupInputComponent() override;
+
+};
